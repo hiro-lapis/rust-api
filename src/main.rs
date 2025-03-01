c9a9c9781ae1f64db55b58c5add0f7dc02341233
hiro-lapis <coolish555ddddd@gmail.com>
2025-03-01 22:04:18 +0900

    implement skelton request handler

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,19 @@
-fn main() {
-    println!("! Hello, world!");
-    println!("global:{}", std::env::var("APP_NAME").unwrap());
-    println!("local:{}", std::env::var("LOCAL").unwrap());
+use std::net::{Ipv4Addr, SocketAddr};
+use axum::{routing::get, Router};
+use tokio::net::TcpListener;
+
+// handler
+async fn hello_world() -> &'static str {
+    "Hello, world!"
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/hello", get(hello_world));
+    let addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 8080);
+
+    let listener = TcpListener::bind(addr).await.unwrap();
+    println!("Listening on {}", addr);
+
+    axum::serve(listener, app).await.unwrap();
 }
