a3a534b57e0601736a655cb2baeeffc5a6aaeab6
hiro-lapis <coolish555ddddd@gmail.com>
2025-03-05 22:43:07 +0900

    add lean error handling

--- api/src/handler/book.rs
+++ api/src/handler/book.rs
@@ -1,5 +1,15 @@
-use axum::{extract::State, http::StatusCode};
+use axum::{
+    extract::{Path, State},
+    http::StatusCode,
+    response::{IntoResponse, Response},
+    Json,
+};
 use registry::AppRegistry;
+use thiserror::Error;
+use uuid::Uuid;
+
+use crate::model::book::{BookResponse, CreateBookRequest};
+
 
 pub async fn register_book(registry: State<AppRegistry>) -> StatusCode {
     todo!()
@@ -9,4 +19,15 @@ pub async fn show_book_list(registry: State<AppRegistry>) -> StatusCode {
 }
 pub async fn show_book(registry: State<AppRegistry>) -> StatusCode {
     todo!()
+}
+
+#[derive(Error, Debug)]
+pub enum AppError {
+    #[error("{0}")]
+    InternalError(#[from] anyhow::Error),
+}
+impl IntoResponse for AppError {
+    fn into_response(self) -> Response {
+        (StatusCode::INTERNAL_SERVER_ERROR, "").into_response()
+    }
 }
