09cea6ef8572b96ea14412c8cda2ad6a87c0fa73
hiro-lapis <coolish555ddddd@gmail.com>
2025-03-06 00:06:21 +0900

    add comment

--- shared/src/env.rs
+++ shared/src/env.rs
@@ -10,11 +10,13 @@ pub enum Environment {
 }
 
 pub fn which() -> Environment {
+    // set default_env based on debug_assertions attribute on build cmd
     #[cfg(debug_assertions)]
     let default_env = Environment::Development;
     #[cfg(not(debug_assertions))]
     let default_env = Environment::Production;
 
+    // read ENV or debug_assertions
     match env::var("ENV") {
         Err(_) => default_env,
         Ok(v) => v.parse().unwrap_or(default_env),
