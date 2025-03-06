e355ccc3a50d88f9221ad03235285ea47855d171
hiro-lapis <coolish555ddddd@gmail.com>
2025-03-07 00:23:44 +0900

    add users and roles

--- adapter/migrations/20250304035325_start.down.sql
+++ adapter/migrations/20250304035325_start.down.sql
@@ -2,5 +2,8 @@
 -- make sure write statements invert order
 DROP TRIGGER IF EXISTS books_updated_at_trigger;
 DROP IF EXISTS books;
+DROP TRIGGER IF EXISTS users_updated_at_trigger;
+DROP IF EXISTS users;
+DROP IF EXISTS roles;
 
 DROP FUNCTION set_updated_at;
