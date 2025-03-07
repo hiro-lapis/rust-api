6f53355a8c07b27955f45e2d5f1c23d993f16f25
hiro-lapis <coolish555ddddd@gmail.com>
2025-03-07 22:39:59 +0900

    add redis connector in kernel

--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -1,2 +1,3 @@
+pub mod auth;
 pub mod model;
 pub mod repository;
---
+++ kernel/src/repository/auth.rs
@@ -0,0 +1,25 @@
+use async_trait::async_trait;
+use shared::error::AppResult;
+
+use crate::model::{
+    auth::{event::CreateToken, AccessToken},
+    id::UserId,
+};
+
+
+#[async_trait]
+pub trait AuthRepository: Send + Sync {
+    async fn fetch_user_id_from_token(
+        &self,
+        access_token: &AccessToken,
+    ) -> AppResult<Option<UserId>>;
+
+    async fn verify_user(
+        &self,
+        email: &str,
+        password: &str,
+    ) -> AppResult<UserId>;
+
+    async fn createToken(&self, access_token: CreateToken) -> AppResult<AccessToken>;
+    async fn deleteToken(&self, access_token: AccessToken) -> AppResult<()>;
+}
