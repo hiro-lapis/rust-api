f9412d94ac7d5890feb50efd0cf7ae2200025c7b
hiro-lapis <coolish555ddddd@gmail.com>
2025-03-10 23:12:39 +0900

    add checkout information in book's api

--- api/src/model/book.rs
+++ api/src/model/book.rs
@@ -1,12 +1,13 @@
-use super::user::BookOwner;
+use super::user::{BookOwner, CheckoutUser};
+use chrono::{DateTime, Utc};
 use derive_new::new;
 use garde::Validate;
 use kernel::model::{
     book::{
         event::{CreateBook, UpdateBook},
-        Book, BookListOptions,
+        Book, BookListOptions, Checkout,
     },
-    id::{BookId, UserId},
+    id::{BookId, CheckoutId, UserId},
     list::PaginatedList,
 };
 use serde::{Deserialize, Serialize};
@@ -142,6 +143,7 @@ pub struct BookResponse {
     pub isbn: String,
     pub description: String,
     pub owner: BookOwner,
+    pub checkout: Option<BookCheckoutResponse>,
 }
 
 // from kernel's model struct to api's model struct
@@ -154,6 +156,7 @@ impl From<Book> for BookResponse {
             isbn,
             description,
             owner,
+            checkout,
         } = value;
 
         Self {
@@ -163,6 +166,30 @@ impl From<Book> for BookResponse {
             isbn,
             description,
             owner: owner.into(), // into BookOwner trait of kernel
+            checkout: checkout.map(BookCheckoutResponse::from),
+        }
+    }
+}
+
+#[derive(Debug, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct BookCheckoutResponse {
+    pub id: CheckoutId,
+    pub checked_out_by: CheckoutUser,
+    pub checked_out_at: DateTime<Utc>,
+}
+
+impl From<Checkout> for BookCheckoutResponse {
+    fn from(value: Checkout) -> Self {
+        let Checkout {
+            checkout_id,
+            checked_out_at,
+            checked_out_by,
+        } = value;
+        Self {
+            id: checkout_id,
+            checked_out_at,
+            checked_out_by: checked_out_by.into(),
         }
     }
 }
